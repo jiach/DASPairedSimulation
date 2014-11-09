b8f7523a1af050e3fc3cad01ce5d9c0bc3aaa76b
jiach <jiacheng@mail.med.upenn.edu>
2014-11-09 15:49:35 -0500

    jiacheng

--- DASPairedSimu.py
+++ DASPairedSimu.py
@@ -4,14 +4,19 @@ import os
 import simu_pairdas
 
 cwd = os.getenv("HOME") + "/IdeaProjects/DASPairedSimulation/"
-pct = 20
-
+pct_gene_picked = 20
+num_sub = 10
 #print set([x[0] for x in genes_picked[0]]).intersection(set([x[0] for x in genes_picked[1]]))
 #print gene_arr.fetch_gene("IL1RN")
 
 # a = simu_pairdas.RefSeqParser(cwd + "refgene")
 gene_arr = simu_pairdas.GeneArrReader(cwd + "allresult_filtered.txt")
-genes_picked = simu_pairdas.pick_null_alt_genes(gene_arr.gene_arr, pct)
+genes_picked = simu_pairdas.pick_null_alt_genes(gene_arr.gene_arr, pct_gene_picked)
+
 # print simu_pairdas.fetch_gene_iso_comp(gene_arr.gene_arr, genes_picked[1])
 #print "\n".join([str(x) for x in simu_pairdas.add_dither_to_isocomp(simu_pairdas.fetch_gene_iso_comp(gene_arr, genes_picked[1]), 10)])
+
+
+null_isocomp_arr = simu_pairdas.add_dither_to_isocomp(simu_pairdas.fetch_gene_iso_comp(gene_arr, genes_picked[0]), num_sub)
+alt_isocomp_arr = simu_pairdas.add_dither_to_isocomp(simu_pairdas.fetch_gene_iso_comp(gene_arr, genes_picked[1]), num_sub)
 
--- simu_pairdas/picker_null_alt_genes.py
+++ simu_pairdas/picker_null_alt_genes.py
@@ -7,50 +7,57 @@ from parser_isoform_comp import *
 def pick_null_alt_genes(gene_arr, pct):
     """
 
-    :rtype : list of lists of strings
-    list[0] = ['name', ... ] for 0 - pct^th percentile of the gene average hell d
-    list[1] = ['name', ... ] for (100-pct)^th-100^th percentile of the gene average hell d
-    """
+   :rtype : list of lists of strings
+   list[0] = ['name', ... ] for 0 - pct_picked_gene^th percentile of
+the gene average hell d
+   list[1] = ['name', ... ] for (100-pct_picked_gene)^th-100^th
+percentile of the gene average hell d
+   """
     gene_max_helld = {}
     gene_min_helld = {}
 
     for key, value in gene_arr.iteritems():
-        # hellDistList = value.getRenyiDiv(3)
         min_max = value.getMinMaxHellingerDistance()
         gene_max_helld[key] = min_max['max']
         cur_min_helld = min_max['min']
         if cur_min_helld[0] >= 0:
             gene_min_helld[key] = cur_min_helld
 
-    sorted_gene_by_max_helld = sorted(gene_max_helld.items(), key=operator.itemgetter(1))
-    sorted_gene_by_min_helld = sorted(gene_min_helld.items(), key=operator.itemgetter(1))
+    sorted_gene_by_max_helld = [list(x) for x in
+                                sorted(gene_max_helld.items(), key=operator.itemgetter(1))]
+    sorted_gene_by_min_helld = [list(x) for x in
+                                sorted(gene_min_helld.items(), key=operator.itemgetter(1))]
     num_picked = int(len(sorted_gene_by_max_helld) * pct / 100)
 
     first_pct = sorted_gene_by_min_helld[0:num_picked]
     last_pct = sorted_gene_by_max_helld[-num_picked:]
 
-    # first_names = set([x[0] for x in first_pct])
+    first_names = set([x[0] for x in first_pct])
 
-    # for i in range(len(last_pct)):
-    #     if last_pct[i][0] in first_names:
-    #         last_pct[i][0] += "_1"
+    for i in range(len(last_pct)):
+        if last_pct[i][0] in first_names:
+            last_pct[i][0] += "_duplicate"
 
     return [first_pct, last_pct]
 
 
 def fetch_gene_iso_comp(gene_arr, genes_picked):
     """
-
-    :rtype : object
-    :param gene_arr: gene_arr obj generated from read_in_gene_arr function,
-                     a dict() with gene name as key, and IsoformPropParser objects as value
-    :param genes_picked: gene list[0] or list[1] generated from pick_null_alt_genes function,
-    :return: array of iso_comp
-    """
-
-    isocomp_arr = []
+   :rtype : dict
+   :param gene_arr: gene_arr obj generated from read_in_gene_arr function,
+                    a dict() with gene name as key, and
+IsoformPropParser objects as value
+   :param genes_picked: gene list[0] or list[1] generated from
+pick_null_alt_genes function,
+   :return: array of iso_comp
+   """
+    isocomp_arr = {}
     for i in range(len(genes_picked)):
-        isocomp_arr.append(gene_arr.gene_arr[genes_picked[i][0]].getIsoComp(genes_picked[i][1][1]))
+        if genes_picked[i][0].endswith("_duplicate"):
+            isocomp_arr[genes_picked[i][0]] = gene_arr.gene_arr[genes_picked[i][0].rstrip("_duplicate")].getIsoComp(
+                genes_picked[i][1][1])
+        else:
+            isocomp_arr[genes_picked[i][0]] = gene_arr.gene_arr[genes_picked[i][0]].getIsoComp(genes_picked[i][1][1])
     return isocomp_arr
 
 
@@ -60,18 +67,17 @@ def add_dither_to_isocomp(isocomp_arr, num_subjects):
     :param num_subjects: sample size that you need to multiply the original isocomp by.
     :return:  [[]]*num_subjects. inner [] =[[iso_name],[before],[after]]
     """
-    dithered_isocomp_arr = []
-    for i in range(len(isocomp_arr)):
-        dithered_isocomp_arr.append(normalize_dither_compositions(isocomp_arr[i], num_subjects))
+    dithered_isocomp_arr = {}
+    for i in isocomp_arr.keys():
+        dithered_isocomp_arr[i]=normalize_dither_compositions(isocomp_arr[i], num_subjects)
     return dithered_isocomp_arr
 
 
 def normalize_dither_compositions(mat, num_subjects):
-
-    arr_isocomp_bysub = [[]]*num_subjects
+    arr_isocomp_bysub = [[]] * num_subjects
 
     for t in range(num_subjects):
-        iso_name = [""]*len(mat[0])
+        iso_name = [""] * len(mat[0])
         before = [0] * len(mat[0])
         after = [0] * len(mat[0])
 
@@ -85,6 +91,7 @@ def normalize_dither_compositions(mat, num_subjects):
             after[i] = after[i] / sum(after)
         arr_isocomp_bysub[t] = [iso_name, before, after]
     return arr_isocomp_bysub
+
 
 class GeneArrReader:
     def __init__(self, fname):
